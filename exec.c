6a0b72737c28787cde09fe344fe4d49b78a735bd
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-05-20 09:23:32 +0200

    heredocs

--- exec.c
+++ exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:06:49 by jeberle           #+#    #+#             */
-/*   Updated: 2024/05/17 08:05:11 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/05/20 07:18:21 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,73 +15,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int	starts_and_ends_with(char *str, char c) {
-	int len = ft_strlen(str);
-	return len > 1 && str[0] == c && str[len - 1] == c;
-}
-int	strictstarts_and_ends_with(char *str, char c) {
-	int len = ft_strlen(str);
-	return len > 1 && str[0] == c && str[len - 1] == c && (str[1] != c || str[len - 2] != c);
-}
-
-char* check_and_extract_quotes(const char* input) {
-	// Länge des Eingabestrings ermitteln
-	size_t len = strlen(input);
-		
-	// Wenn die Länge kleiner als 2 ist, gibt es keine passenden Anführungszeichen
-	if (len < 2) {
-		return strdup("");
-	}
-
-	// Zeiger auf den Anfang und das Ende des Strings
-	const char *start = input;
-	const char *end = input + len - 1;
-
-	// Speicher für die resultierenden Anführungszeichen allokieren
-	char *result = malloc(len + 1);
-	if (result == NULL) {
-		fprintf(stderr, "Speicherzuweisung fehlgeschlagen\n");
-		return strdup("");
-	}
-	size_t result_index = 0;
-
-	// Überprüfen der verschachtelten Anführungszeichen
-	while (start < end) {
-		if ((*start == '"' && *end == '"') || (*start == '\'' && *end == '\'')) {
-			result[result_index++] = *start;
-			start++;
-			end--;
-		} else {
-			break;
-		}
-	}
-
-	// Nullterminator hinzufügen
-	result[result_index] = '\0';
-
-	// Falls keine verschachtelten Anführungszeichen gefunden wurden, leeren String zurückgeben
-	if (result_index == 0) {
-		free(result);
-		return ft_strdup("");
-	}
-
-	return result;
-}
-
-char *ft_quote(char *str)
+char	*ft_quote(char *str)
 {
-	char *quote;
-	size_t len;
-	size_t str_len;
+	char	*quote;
+	size_t	len;
+	size_t	str_len;
 
 	if (!str)
 		return (NULL);
-
 	str_len = ft_strlen(str);
-
 	if (str[0] == '"' && str[str_len - 1] == '"')
-		return ft_strdup(str);
-
+		return (ft_strdup(str));
 	len = str_len + 3;
 	quote = (char *)malloc(len * sizeof(char));
 	if (!quote)
@@ -90,51 +34,7 @@ char *ft_quote(char *str)
 	ft_strcpy(&quote[1], str);
 	quote[len - 2] = '"';
 	quote[len - 1] = '\0';
-
 	return (quote);
-}
-
-
-char	**ft_exc_args_awk(char *full_command, char **envp)
-{
-	char	**spaceargs;
-	char	**args;
-	char	*concat;
-	char	*tmp;
-	int		i;
-
-	i = 1;
-	args = malloc(3 * sizeof(char *));
-	if (args == NULL)
-		return (NULL);
-	spaceargs = ft_split(full_command, ' ');
-	args[0] = ft_exc_path(retrieve_bsc_command(full_command, "/", ""), envp);
-	concat = ft_strdup("");
-	while (i < ft_array_length(spaceargs) && spaceargs[i])
-	{
-		tmp = ft_strjoin(concat, " ");
-		free(concat);
-		concat = ft_strjoin(tmp, spaceargs[i]);
-		free(tmp);
-		i++;
-	}
-		ft_printf("test\n");
-		ft_printf("ARG: %s\n", concat);
-		ft_putstr(concat);
-		ft_putstr(check_and_extract_quotes(concat));
-	if (ft_strcmp("'", check_and_extract_quotes(concat)) == 0) {
-		ft_printf("test\n");
-		// Test 7
-		args[1] = ft_quote(ft_strtrim(concat, " '"));
-	} else if (!starts_and_ends_with(concat, '"') && !starts_and_ends_with(concat, '\'')) {
-		args[1] = ft_strtrim(concat, "\" '");
-	} else if (strstr(full_command, "Test 8") != NULL) {
-		// Test 8
-		args[1] = ft_quote(ft_strtrim(concat, "' "));
-	}
-	args[2] = NULL;
-	ft_array_l_free(spaceargs, ft_array_length(spaceargs));
-	return (args);
 }
 
 char	*ft_get_envline(char *needle, char **envp)
@@ -192,12 +92,9 @@ char	*retrieve_bsc_command(char *full_cmd, char *prefix, char *suffix)
 	return (command);
 }
 
-char	**ft_exc_args(char *full_cmd, char *command, char **envp)
+char	**ft_exc_args(char *full_cmd)
 {
-	if (ft_strcmp(command, "awk") == 0)
-		return (ft_exc_args_awk(full_cmd, envp));
-	else
-		return (ft_split(full_cmd, ' '));
+	return (ft_split(full_cmd, ' '));
 }
 
 int	ft_execve(char *command, char **envp)
@@ -206,7 +103,7 @@ int	ft_execve(char *command, char **envp)
 	char	**args;
 
 	path = ft_exc_path(retrieve_bsc_command(command, "/", ""), envp);
-	args = ft_exc_args(command, retrieve_bsc_command(command, "", ""), envp);
+	args = ft_exc_args(command);
 	if (execve(path, args, envp) == -1)
 	{
 		ft_putstr_fd(STDERR_FILENO, "Error executing:\n");
--- pipex.c
+++ pipex.c
@@ -6,30 +6,94 @@
 /*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 13:43:03 by jeberle           #+#    #+#             */
-/*   Updated: 2024/05/18 21:39:53 by jonathanebe      ###   ########.fr       */
+/*   Updated: 2024/05/20 09:18:32 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/pipex.h"
 
-#include "./includes/pipex.h"
+int	save_hd_input(char **argv)
+{
+	char	buffer[BUFFER_SIZE];
+	int		readchar;
+	int		tmp;
+
+	tmp = open("hd.tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (tmp == -1)
+		return (-1);
+	while (1)
+	{
+		write(STDOUT_FILENO, "heredoc> ", 9);
+		readchar = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1);
+		if (readchar == -1)
+			return (-1);
+		buffer[readchar] = '\0';
+		if (ft_strncmp(buffer, argv[2], ft_strlen(argv[2])) == 0)
+		{
+			if (buffer[ft_strlen(argv[2])] == '\n')
+				break ;
+		}
+		write(tmp, buffer, readchar);
+	}
+	close(tmp);
+	return (0);
+}
 
 int	open_in_out(int argc, char **argv, int *fd_in, int *fd_out)
 {
-	if ()
+	if (ft_strcmp(argv[1], "here_doc") == 0)
 	{
+		if (save_hd_input(argv) == -1)
+			return (-1);
+		*fd_in = open("hd.tmp", O_RDONLY);
+		if (*fd_in == -1)
+			return (-1);
 	}
 	else
 	{
 		*fd_in = open(argv[1], O_RDONLY);
-		*fd_out = open(argv[(argc - 1)], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (*fd_in == -1 || *fd_out == -1)
+		if (*fd_in == -1)
 			return (-1);
+	}
+	*fd_out = open(argv[(argc - 1)], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (*fd_out == -1)
+	{
+		close(*fd_in);
+		return (-1);
 	}
 	return (0);
 }
 
-// TODO here_docs
+int	get_initial_index(char **argv)
+{
+	if (ft_strcmp(argv[1], "here_doc") == 0)
+		return (3);
+	return (2);
+}
+
+void	wait_handling(int argc, char **argv, int processes[])
+{
+	int	i;
+
+	i = 0;
+	if (ft_strcmp(argv[1], "here_doc") == 0)
+	{
+		while (i < argc - 4)
+		{
+			waitpid(processes[i], NULL, 0);
+			i++;
+		}
+	}
+	else
+	{
+		while (i < argc - 3)
+		{
+			waitpid(processes[i], NULL, 0);
+			i++;
+		}
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int	fd_in;
@@ -47,11 +111,7 @@ int	main(int argc, char **argv, char **envp)
 	}
 	if (open_in_out(argc, argv, &fd_in, &fd_out) == -1)
 		return (ft_putstr_fd(STDERR_FILENO, "Error opening file\n"), 1);
-
-	if (ft_strcmp(argv[1], "here_doc") == 0)
-		i = 3; /* Skip "here_doc" and LIMITER */
-	else
-		i = 2;
+	i = get_initial_index(argv);
 	prevpipe = fd_in;
 	while (i < (argc - 1))
 	{
@@ -79,7 +139,10 @@ int	main(int argc, char **argv, char **envp)
 		}
 		else
 		{
-			processes[i - 2] = process;
+			if (ft_strcmp(argv[1], "here_doc") == 0)
+				processes[i - 3] = process;
+			else
+				processes[i - 2] = process;
 			close(tube[1]);
 			if (prevpipe != fd_in)
 				close(prevpipe);
@@ -91,11 +154,6 @@ int	main(int argc, char **argv, char **envp)
 		close(prevpipe);
 	close(fd_in);
 	close(fd_out);
-	i = 0;
-	while (i < argc - 3)
-	{
-		waitpid(processes[i], NULL, 0);
-		i++;
-	}
+	wait_handling(argc, argv, processes);
 	return (0);
 }
