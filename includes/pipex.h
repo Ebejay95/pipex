fdbb79345688887b0435b71d1a1f518d26c69151
Jonathan Eberle <jonathan.eberle@gmx.de>
2024-05-24 10:01:46 +0200

    fixed definition error

--- includes/pipex.h
+++ includes/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 13:43:52 by jeberle           #+#    #+#             */
-/*   Updated: 2024/05/23 19:45:41 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/05/24 09:46:55 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ typedef struct s_processes
 char	*ft_get_envline(char *needle, char **envp);
 char	*ft_exc_path(char *exc, char **envp);
 char	*retrieve_bsc_command(char *full_cmd, char *prefix, char *suffix);
-char	**ft_exc_args(char *full_cmd);
+char	**ft_exc_args(char *full_cmd, int *arglen);
 int		ft_execve(char *command, char **envp);
 int		wait_handling(t_args *args, t_processes *prcs);
 int		child_process(t_fds *fds, t_args *args, char **envp);
